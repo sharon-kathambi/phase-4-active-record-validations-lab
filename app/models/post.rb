6c583de77a2529bf7d26ffc7fdc15432a6694950
Sharon Mwenda <smkathambi@gmail.com>
2022-10-05 14:37:08 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+
+
+    TITLE_NOT_CONTAIN = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top/i,
+        /Guess/i
+
+    ] 
+   
+    def title_is_clickbait
+        if TITLE_NOT_CONTAIN.none? {|word| word.match title}
+         errors.add(:title, "Invalid title")
+        end
+     end
+
 end
